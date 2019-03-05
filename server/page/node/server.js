165f1ac038aed52fd74931aa1019b53d119c8e43
Tobi Dæ <rorgate@gmail.com>
2019-03-05 09:23:31 +0100

    user id

--- server/page/node/server.js
+++ server/page/node/server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const redis = require('redis');
 const fs = require('fs');
 const enableWs = require('express-ws')
+const crypto = require('crypto')
 
 // const https = require('https');
 // const helmet = require('helmet');
 
 const app = express();
 enableWs(app);
-// const rclient = redis.createClient({host: 'redis', port: 6379});
-// rclient.on('connect', () => {
-//   console.log('Connected to redis!');
-//   rclient.set('pep8request-counter', 0);
-// });
-// rclient.on('error', (err) => {
-//   console.log('Error connecting to redis: ' + err);
-// });
+const rclient = redis.createClient({host: 'localhost', port: 6379});
+rclient.on('connect', () => {
+  console.log('Connected to redis!');
+  rclient.set('pep8request-counter', 0);
+});
+rclient.on('error', (err) => {
+  console.log('Error connecting to redis: ' + err);
+});
 
 // certs
 // const credentials = {
@@ -33,6 +35,31 @@ const staticfileoptions = {
 };
 
 // app.use(helmet());
+app.use(cookieParser());
+// cookies
+// app.use((req, res, next) => {
+//   var cookie = req.cookies.cookieName;
+//   if (cookie === undefined) {
+//     // user doesn't exist
+//     while (true) {
+//       const sha = crypto.createHash('sha256');
+//       sha.update(Math.random().toString());
+//       var uid = 'user:' + sha.digest('hex');
+//       console.log(uid);
+//       console.log(rclient.exists(uid));
+//       break;
+//       if (!rclient.exists(uid)) {
+//         console.log(rclient.hset(uid, '{}'));
+//         break;
+//       }
+//     }
+//     console.log('New user uid:', uid);
+//   } else {
+//     // user exists
+//     console.log('User exists!');
+//   }
+//   next();
+// });
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(express.static(staticfileoptions['root'], staticfileoptions));
@@ -44,56 +71,20 @@ app.get('/', (req, res) => {
       console.log(err);
       res.status(err.status).end();
     } else {
-      // rclient.incr('page-counter', (err, cnt) => {
-      //   console.log('Page count: ' + cnt.toString());
-      // });
+      rclient.incr('page-counter', (err, cnt) => {
+        console.log('Page count: ' + cnt.toString());
+      });
     }
   });
 });
 
-// explicit router for cert challenge -- TODO
-/*app.get('/.well-known/acme-challenge*', (req, res) => {
-  res.sendFile(req.url, staticfileoptions, (err) => {
-    if (err) {
-      console.log("Error while trying acme-challenge:");
-      console.log(err);
-      res.status(err.status).end();
-    }
-  });
-});*/
-
-// app.post('/2pep', (req, res) => {
-//   rclient.incr('pep8request-counter', (err, uniqueID) => {
-//     const rSubClient = redis.createClient({host: 'redis', port: 6379});
-//     rSubClient.subscribe('pep8result#' + uniqueID.toString());
-//     rSubClient.on('message', (chan, msg) => {
-//         var job = JSON.parse(msg);
-//         console.log('Job #' + uniqueID.toString() + ' done!');
-//         res.send(msg);
-//       });
-//     rSubClient.on('connect', () => {
-//       rclient.lpush('pep8jobs',
-//         JSON.stringify({'id': uniqueID,
-//                         'lines': req.body.txt,
-//                         'select': req.body.sel
-//       }));
-//       console.log('Submitted job #' + uniqueID.toString() + '!');
-//     });
-//     rSubClient.on('error', (err) => {
-//       console.log('Subclient #' + uniqueID.toString() + ': error connecting to redis: ' + err);
-//     });
-//   });
-// })
-
-var clients = [];
-var keyboards = [];
-
 app.ws('/client', (wsC, req) => {
   console.log('ws connection to client established');
-  clients.push({'wsC': wsC, 'id': clients.length})
+  rclient.lpush({'wsC': wsC, 'id': clients.length})
+
   wsC.on('message', (msg) => {
-    console.log('client: got "' + msg.utf8Data + '"');
-    keyboards[0].send(msg.utf8Data);
+    console.log('client: got "' + msg + '"');
+    keyboards[0].send(msg);
   });
 
   wsC.on('close', (conn) => {
